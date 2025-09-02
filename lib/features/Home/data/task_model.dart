547191ee7a15a415274230da19513a5424ad7978
mostafa <mostafarasekh5@gmail.com>
2025-09-02 18:51:17 +0300

    first commit

---
+++ lib/features/Home/data/task_model.dart
@@ -0,0 +1,19 @@
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  String description;
+
+  @HiveField(2)
+  bool isCompleted;
+
+  TaskModel({
+    required this.title,
+    required this.description,
+    this.isCompleted = false,
+  });
+}
